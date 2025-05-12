513f3531f8ef537344922e8a196f2929c6409974
rikuhidaka <riku0826h@gmail.com>
2025-05-12 20:46:35 +0900

    🔧 2025年に

--- app/graph/_custom-graph/sample-data.ts
+++ app/graph/_custom-graph/sample-data.ts
@@ -1,29 +1,29 @@
-export const dateRange = [new Date('2023-01-01'), new Date('2023-01-10')] as const;
+export const dateRange = [new Date('2025-01-01'), new Date('2025-01-10')] as const;
 
 // サンプルデータ（日付と数値）
 export const data1 = [
-  { date: new Date('2023-01-01'), value: 10 },
-  { date: new Date('2023-01-02'), value: 40 },
-  { date: new Date('2023-01-03'), value: 25 },
-  { date: new Date('2023-01-04'), value: 60 },
-  { date: new Date('2023-01-05'), value: 20 },
-  { date: new Date('2023-01-06'), value: 80 },
-  { date: new Date('2023-01-07'), value: 30 },
-  { date: new Date('2023-01-08'), value: 70 },
-  { date: new Date('2023-01-09'), value: 50 },
-  { date: new Date('2023-01-10'), value: 90 },
+  { date: new Date('2025-01-01'), value: 10 },
+  { date: new Date('2025-01-02'), value: 40 },
+  { date: new Date('2025-01-03'), value: 25 },
+  { date: new Date('2025-01-04'), value: 60 },
+  { date: new Date('2025-01-05'), value: 20 },
+  { date: new Date('2025-01-06'), value: 80 },
+  { date: new Date('2025-01-07'), value: 30 },
+  { date: new Date('2025-01-08'), value: 70 },
+  { date: new Date('2025-01-09'), value: 50 },
+  { date: new Date('2025-01-10'), value: 90 },
 ];
 
 // サンプルデータ（日付と数値）
 export const data2 = [
-  { date: new Date('2023-01-01'), value: 40 },
-  { date: new Date('2023-01-02'), value: 10 },
-  { date: new Date('2023-01-03'), value: 55 },
-  { date: new Date('2023-01-04'), value: 60 },
-  { date: new Date('2023-01-05'), value: 90 },
-  { date: new Date('2023-01-06'), value: 70 },
-  { date: new Date('2023-01-07'), value: 60 },
-  { date: new Date('2023-01-08'), value: 70 },
-  { date: new Date('2023-01-09'), value: 60 },
-  { date: new Date('2023-01-10'), value: 40 },
+  { date: new Date('2025-01-01'), value: 40 },
+  { date: new Date('2025-01-02'), value: 10 },
+  { date: new Date('2025-01-03'), value: 55 },
+  { date: new Date('2025-01-04'), value: 60 },
+  { date: new Date('2025-01-05'), value: 90 },
+  { date: new Date('2025-01-06'), value: 70 },
+  { date: new Date('2025-01-07'), value: 60 },
+  { date: new Date('2025-01-08'), value: 70 },
+  { date: new Date('2025-01-09'), value: 60 },
+  { date: new Date('2025-01-10'), value: 40 },
 ];
